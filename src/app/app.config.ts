0c7ba1d7fe7e30ae58edd015abd66f341c49631e
yash-mandali <yashmandali211@gmail.com>
2026-03-20 15:56:24 +0530

    final working

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
 
